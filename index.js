4387acad82de16342ee290e146fe83ab80a05c50
Rodney <rodneypa@LAPTOP-KQJ5G51E.localdomain>
2022-02-02 13:58:46 -0500

    commit tree 091d94b12e71de96e22798003f189800e6f10677

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function superbowlWin (record){
+if (record.find(findW) === undefined) {
+        return undefined
+    }
+ else {
+    return record.find(findW).year
+ }
+
+    
+}
+
+function findW(element) {
+return element.result === "W"
+}
+
+//superbowlWin.find(record);
+//onsole.log(record.year);
